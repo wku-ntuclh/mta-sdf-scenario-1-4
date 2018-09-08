7939824474888872c1ee3e2040046efa2218e775
William Ku <kucheech@gmail.com>
2018-09-08 21:02:58 +0800

    implemented recursirve function to determine number of significant digits

--- Scenario 1-4/Program.cs
+++ Scenario 1-4/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Scenario_1_4
 {
@@ -10,6 +6,39 @@ namespace Scenario_1_4
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            String n = Console.ReadLine();
+            Console.WriteLine(getSignificantDigits(n));
+        }
+
+        static int getSignificantDigits(String n)
+        {
+            if(n == "0")
+            {
+                return 0;
+            } else if (n.Length == 1)
+            {
+                return 1;
+            } else
+            {                
+                int result = getSignificantDigits(n.Substring(1));
+
+                int d = n[0] - '0';
+                if(d == 0)
+                {
+                    if(result > 0)
+                    {
+                        return result + 1;
+                    } else
+                    {
+                        return 0;
+                    }
+                } else
+                {
+                    return result + 1;
+                }
+            }
+
         }
     }
 }
